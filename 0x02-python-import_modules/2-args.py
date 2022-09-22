213d527c8f9cd60ff92a93652206c44a2065f2e6
Yidu-xSkript <yidu95@gmail.com>
2022-09-22 08:41:10 -0700

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    argL = len(argv)
-    print("{:d} {:s}{:s}".format(argL - 1, "argument" if argL <= 2 else "arguments", 
-        "." if argL == 1 else ":"))
+    al = len(argv)
+    print("{:d} {:s}{:s}".format(al - 1,
+                                 "argument" if al <= 2 else "arguments",
+                                 "." if al == 1 else ":"))
     for i, s in enumerate(argv):
         if i > 0:
             print("{:d}: {:s}".format(i, s))
