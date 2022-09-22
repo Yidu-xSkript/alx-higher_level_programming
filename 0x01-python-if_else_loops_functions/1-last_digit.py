5e85e98dbcb0b04afdaed0a304da8d731007a2fd
Yidu-xSkript <yidu95@gmail.com>
2022-09-22 17:11:57 +0300

    last digit ex - pycodestyle fix

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,13 +2,13 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
-    number_int = -(abs(number) % 10)
+    num = -(abs(number) % 10)
 else:
-    number_int = abs(number) % 10
+    num = abs(number) % 10
 
-if number_int > 5:
-    print(f"Last digit of {number} is {number_int} and is greater than 5")
-elif number_int == 0:
-    print(f"Last digit of {number} is {number_int} and is 0")
-elif number_int < 6 and number_int != 0:
-    print(f"Last digit of {number} is {number_int} and is less than 6 and not 0")
+if num > 5:
+    print(f"Last digit of {number} is {num} and is greater than 5")
+elif num == 0:
+    print(f"Last digit of {number} is {num} and is 0")
+elif num < 6 and num != 0:
+    print(f"Last digit of {number} is {num} and is less than 6 and not 0")
