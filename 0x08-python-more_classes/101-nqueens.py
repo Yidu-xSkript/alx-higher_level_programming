4dd3b8a8cb24b07e2ed071b29144cec510702cd6
Yidu-xSkript <yidu95@gmail.com>
2022-10-17 19:39:44 +0300

    number

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -5,11 +5,11 @@ import sys
 """ Python3 program to solve N Queen Problem using 
 backtracking """
 
-if len(sys.args) is not 2:
+if len(sys.argv) != 2:
     print("Usage: nqueens N")
     exit(1)
 
-N = sys.args[1]
+N = int(sys.argv[1])
 
 if type(N) is not int:
     print("N must be a number")
