216db281e4b9b71933e86845a8c0be4bdc6c6bad
Yidu-xSkript <yidu95@gmail.com>
2022-10-17 19:50:14 +0300

    minor fixes

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -9,11 +9,11 @@ if len(sys.argv) != 2:
     print("Usage: nqueens N")
     exit(1)
 
-N = int(sys.argv[1])
-
-if type(N) is not int:
+if not sys.argv[1].isdigit():
     print("N must be a number")
     exit(1)
+
+N = int(sys.argv[1])
 
 if N < 4:
     print("N must be at least 4")
