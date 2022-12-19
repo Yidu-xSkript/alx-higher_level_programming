806b74a552b1bfd152fbe404e457bf27afa22de7
Yidu-xSkript <yidu95@gmail.com>
2022-12-19 22:36:02 +0300

    semistandard fix

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const arg = process.argv.slice(2);
-if (arg.length == 1) {
-    console.log('Argument Found');
+if (arg.length === 1) {
+  console.log('Argument Found');
 } else if (arg.length > 1) {
-    console.log('Arguments Found');
+  console.log('Arguments Found');
 } else {
-    console.log('No Argument');
+  console.log('No Argument');
 }
