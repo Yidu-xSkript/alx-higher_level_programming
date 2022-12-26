f440d6c32ea45ab1d3b9a5a88326ae047da9e2ba
Yidu-xSkript <yidu95@gmail.com>
2022-12-26 10:49:27 +0300

    done

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-
+""" lists all states from the database hbtn_0e_0_usa
+"""
 import MySQLdb
 import sys
 
@@ -7,16 +8,15 @@ username = sys.argv[1]
 password = sys.argv[2]
 dbname = sys.argv[3]
 
-try:
-    db = MySQLdb.connect("localhost:3306", username, password, dbname)
-except:
-    print("Can't connect to db")
-    return 0
 
-cursor = db.cursor()
-cursor.execute("SELECT name FROM states")
-results = cursor.fetchall()
+if __name__ == "__main__":
+    db = MySQLdb.connect("localhost", 3306, username, password, dbname, charset="utf8")
 
-print(results)
+    cursor = db.cursor()
+    cursor.execute("SELECT name FROM states ORDER BY id ASC")
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
 
-db.close()
+    cursor.close()
+    db.close()
