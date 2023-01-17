d0e1fc7aebc6eff89672cda585616c896e09c850
Yidu-xSkript <yidu95@gmail.com>
2023-01-17 03:37:15 -0800

    sends a request to the URL and displays the value of the X-Request-Id variable found in the header of the response

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""
+takes a url,
+displays value of X-Request-Id variable
+found in the header of the response.
+"""
+
+import sys
+import urllib.request
+
+if __name__ == "__main__":
+	url = sys.argv[1]
+
+	with urllib.request.urlopen(url) as res:
+		print(dict(res.headers).get("X-Request-Id"))
