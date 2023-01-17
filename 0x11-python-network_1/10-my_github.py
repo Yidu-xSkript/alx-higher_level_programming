a1713e9ce977c918066de2b3eafc08f3a2acd431
Yidu-xSkript <yidu95@gmail.com>
2023-01-17 04:56:17 -0800

    Get ID using github credentials

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""
+Takes Github Credentials
+use github API to display ID
+"""
+
+import sys
+import requests
+from requests.auth import HTTPBasicAuth
+
+if __name__ == "__main__":
+	auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+	response = requests.get("https://api.github.com/user", auth=auth)
+	print(response.json().get("id"))
