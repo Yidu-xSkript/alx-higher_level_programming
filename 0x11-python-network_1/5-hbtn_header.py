170c348381f80e6ac58d3b47864bb4a312c73842
Yidu-xSkript <yidu95@gmail.com>
2023-01-17 04:23:47 -0800

    X-Request-Id using requests

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""
+Display X-Request-Id from body response using requests lib
+"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+	res = requests.get(sys.argv[1])
+	print(res.headers.get("X-Request-Id"))
