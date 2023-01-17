9ceb6abcdd89b2721ab27c3dbe21b508b176e5c5
Yidu-xSkript <yidu95@gmail.com>
2023-01-17 04:29:47 -0800

    send email as parameter using requests lib

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""
+POST request using requests lib
+send email parameter to url
+"""
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+	response = requests.post(sys.argv[1], data={'email': sys.argv[2]})
+	print(response.text)
