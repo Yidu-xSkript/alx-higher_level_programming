d987e24535c3e0c6def3a541109caa46ff8bc063
Yidu-xSkript <yidu95@gmail.com>
2023-01-17 04:15:26 -0800

    Error Code

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""
+takes in url,
+sends a request,
+display body response
+"""
+
+import sys
+import urllib.request
+import urllib.error
+
+if __name__ == "__main__":
+	try:
+		with urllib.request.urlopen(sys.argv[1]) as res:
+			print(res.read().decode('UTF-8'))
+	except urllib.error.HTTPError as error:
+		print('Error code:', error.code)
