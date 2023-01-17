6f89a0db16025d4857a131ced27eba87011f0ad3
Yidu-xSkript <yidu95@gmail.com>
2023-01-17 04:20:47 -0800

    print status using request lib

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""
+fetch data using request lib
+"""
+
+import requests
+
+if __name__ == "__main__":
+	res = requests.get('https://alx-intranet.hbtn.io/status')
+	data = res.text
+	print('Body response:')
+	print('\t- type: {}'.format(type(data)))
+	print('\t- content: {}'.format(data))
