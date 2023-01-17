7be67b9b0a0d1d322054a4ea06f2f3f5ad0d0ec5
Yidu-xSkript <yidu95@gmail.com>
2023-01-17 03:49:51 -0800

    post request that takes email as a parameter

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""
+Script that
+- takes in URL & email
+- sends a POST request with the email parameter
+- displays the body response (utf-8)
+"""
+
+import sys
+import urllib.parse
+import urllib.request
+
+if __name__ == "__main__":
+	url = sys.argv[1]
+	payload = urllib.parse.urlencode({"email": sys.argv[2]}).encode('ascii')
+	request = urllib.request.Request(url, payload)
+
+	with urllib.request.urlopen(request) as res:
+		print(res.read().decode('utf-8'))
