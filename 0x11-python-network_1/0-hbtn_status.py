6dc8df8c198615b04bd49e290ca716f5fd8b430a
Yidu-xSkript <yidu95@gmail.com>
2023-01-17 03:26:38 -0800

    task 0 - body response on fetch using urllib

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""A script that fetches https://alx-intranet.hbtn.io/status using urllib"""
+
+if __name__ == "__main__":
+	import urllib.request
+
+	with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+		data = response.read()
+		print("Body response:")
+		print("\t- type: {}".format(type(data)))
+		print("\t- content: {}".format(data))
+		print("\t- utf8 content: {}".format(data.decode('utf-8')))
