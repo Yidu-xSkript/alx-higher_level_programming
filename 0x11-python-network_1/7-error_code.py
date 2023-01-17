7052b3baa303df56fa21b1992e1da4eeb9e8e5e5
Yidu-xSkript <yidu95@gmail.com>
2023-01-17 04:32:40 -0800

    Error Mgmt using request lib

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""
+takes url,
+display body response
+using requests lib
+"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+	res = requests.get(sys.argv[1])
+	if res.status_code >= 400:
+		print("Error Code: {}".format(res.status_code))
+	else:
+		print(res.text)
