655614000dc08fabe04c0cf3303c781774b8da74
Yidu-xSkript <yidu95@gmail.com>
2023-02-20 06:45:47 -0800

    say hello!

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  function getSalut () {
+    return $.get({
+      url: 'https://fourtonfish.com/hellosalut/?lang=fr',
+      dataType: 'json'
+    });
+  }
+
+  getSalut().then((res) => {
+    $('div#hello').text(res.hello);
+  });
+});
